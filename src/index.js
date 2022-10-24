835c55ece688df422580bc8d8b1da3dc72f6b1e0
ndt2804 <100899825+ndt2804@users.noreply.github.com>
2022-10-24 19:57:19 +0700

    create route, controller

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ const morgan = require('morgan');
 const exphdb = require('express-handlebars');
 const { extname } = require('path');
 
+const route = require('./routes');
+
 
 const app = express();
 const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
 //HTTP Logger
 app.use(morgan('combined'));
 //template engine
@@ -16,14 +23,10 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+//route init
+route(app);
 
 
-app.listen(port, () => {
-console.log(`Example app listening on port ${port}`);
-});
+
+
+app.listen(port, () => {console.log(`Example app listening on port ${port}`);});
